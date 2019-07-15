278bf16cc932082d7a22ebf1b34194804871fb3f
Andrey Chernov <achernov@leon.esterholdings.com>
2019-07-15 10:25:00 -0400

    exception

--- index.php
+++ index.php
@@ -7,7 +7,10 @@ use Lernpad\ZApi\Model\StatusMsg;
 use Lernpad\ZApi\Model\UserMsg;
 use Lernpad\ZApi\Model\EventMsg;
 use Lernpad\ZApi\Model\VersionMsg;
+use Lernpad\ZApi\Exception\TimeoutException;
 
+define(API_HOST, "localhost");
+define(API_PORT, "8888");
 
 $authUser = new UserMsg();
 $authUser->setLogin(999);
@@ -15,12 +18,16 @@ $authUser->setPassword('MasterPassword');
 
 // Client Api service
 $cp = new ClientProtocol();
-$cp->connect('98.158.104.229', 20004, $authUser);
+$cp->connect(API_HOST, API_PORT, $authUser);
 
 $appId = 1;
-$result = $cp->versionGet($appId);
-$code = $result['status']->getCode();
-$ver = $result['version'];
-echo "service status(".$code.",".StatusMsg::getName($code).")<br/>\n";
-/* @var $ver VersionMsg */
-echo "version " . $ver->getMajor() . '.' . $ver->getMinor() . '.' . $ver->getPatch() . " URL: " . $ver->getLink() . "<br/>\n";
+try {
+    $result = $cp->versionGet($appId);
+    $code = $result['status']->getCode();
+    $ver = $result['version'];
+    echo "service status(".$code.",".StatusMsg::getName($code).")<br/>\n";
+    /* @var $ver VersionMsg */
+    echo "version " . $ver->getMajor() . '.' . $ver->getMinor() . '.' . $ver->getPatch() . " URL: " . $ver->getLink() . "<br/>\n";
+} catch(Exception $e) {
+    echo get_class($e);
+}
